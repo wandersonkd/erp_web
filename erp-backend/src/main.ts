93707e47be35f2a4be63bbdbc16991773df3d265
wandersonkd <wanderson_alves@msn.com>
2025-09-25 14:18:28 -0300

    inicial
    inicial

---
+++ erp-backend/src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Habilita o uso de DTOs e validações globalmente
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('ERP API')
+    .setDescription('Documentação da API para o sistema ERP')
+    .setVersion('1.0')
+    .addBearerAuth() // Adiciona o campo de autorização para JWT
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // A documentação estará em /api
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+void bootstrap();
